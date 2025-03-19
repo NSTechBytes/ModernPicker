58f8e832b5b110867487be9132a0116a57a59892
NSTechBytes <nstechbytes@gmail.com>
2025-03-19 20:41:42 +0500

    added update)history

---
+++ @Resources/scripts/lua/update_history.lua
@@ -0,0 +1,31 @@
+function updateHistory()
+    -- Get the picked color from the measure
+    local newColor = SKIN:GetMeasure("YourPicker"):GetStringValue()
+
+    -- Get previous colors from variables
+    local history = {}
+    for i = 1, 6 do
+        history[i] = SKIN:GetVariable("History_Color" .. i, "#FFFFFF")
+    end
+
+    -- Shift colors down
+    for i = 6, 2, -1 do
+        history[i] = history[i - 1]
+    end
+    history[1] = newColor
+
+    -- Update variables and write to the skin
+    for i = 1, 6 do
+        SKIN:Bang('!SetVariable', "History_Color" .. i, history[i])
+        SKIN:Bang('!WriteKeyValue', 'Variables', "History_Color" .. i, history[i])
+    end
+
+    -- Apply colors to ellipse meters
+    for i = 1, 6 do
+        SKIN:Bang('!SetOption', "History_Ellipse_Color_" .. i, "This" , "FillColor" .. history[i])
+    end
+
+    -- Refresh the skin meters
+    SKIN:Bang('!UpdateMeter', '*')
+    SKIN:Bang('!Redraw')
+end
