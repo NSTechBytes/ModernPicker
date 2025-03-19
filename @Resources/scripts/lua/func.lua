cc6aed3cdda91463364e77cb19887ecb6687b57e
NSTechBytes <nstechbytes@gmail.com>
2025-03-19 15:56:01 +0500

    added initialize

--- @Resources/scripts/lua/func.lua
+++ @Resources/scripts/lua/func.lua
@@ -1,3 +1,17 @@
+function Initialize()
+    for i = 1, 6 do
+        local color = SKIN:GetVariable('History_Color' .. i)
+        if color == nil or color == '' then
+            SKIN:Bang('!HideMeter', 'History_Ellipse_Color_' .. i)
+            --print('History_Ellipse_Color_' .. i .. ' will be hidden')
+        end
+    end
+    SKIN:Bang('!UpdateMeter', '*')
+    SKIN:Bang('!Redraw')  
+end
+
+
+
 function trim(Text, Match, Replace)
 	return Text:gsub(Match, Replace)
 end
